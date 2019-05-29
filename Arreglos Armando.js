0b949d6be757d7db60e149f9ec30312baaf8641f
Armando Lezama <armando.lezama.psic@gmail.com>
2019-05-28 20:13:12 -0500

    Terminé el primer ejercicio

---
+++ Arreglos Armando.js
@@ -0,0 +1,16 @@
+/*A Daniel le acaban de mandar una lista con sus calificaciones, quiere saber si su promedio es suficiente 
+para tener una beca este año, el promedio mínimo que necesita es 8, el programa recibirá un arreglo de 
+calificaciones para poder calcular el resultado*/
+
+let calificaciones = [0,0,0,0,0,0,0,0];
+let promedio = 0;
+
+for(let i = 0; i < calificaciones.length; i++){
+    promedio += calificaciones[i];
+}
+
+promedio = promedio/calificaciones.length
+
+//Promedio obtenido
+
+
