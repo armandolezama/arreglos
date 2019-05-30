5a39c689893f6eee66d11a663783bcc14c3530b0
Armando Lezama <armando@clivoeq.com>
2019-05-30 16:36:55 -0500

    Realicé funciones para registrar en un arreglo un objeto que contuviera una variable nombre y a su vez un arreglo bidimensional con materias y calificaciones, falta una función que regrese las materias reprobadas de cada alumno

--- Funciones auxiliares.js
+++ Funciones auxiliares.js
@@ -1,16 +1,16 @@
 function lasCalificaciones(nombre){
 
-    let cantidadDeCalificaciones = parseInt(prompt(`¿Cuántas calificaciones tiene ${nombre}`))
+    let cantidadDeCalificaciones = parseInt(prompt(`¿Cuántas calificaciones tiene ${nombre}`));
 
     while(isNaN(cantidadDeCalificaciones)){
-        cantidadDeCalificaciones = parseInt(prompt("Eso no es un número, por favor, introduce uno válido"))
+        cantidadDeCalificaciones = parseInt(prompt("Eso no es un número, por favor, introduce uno válido"));
     }
     let calificaciones = [];
     //Terminar bucle for
     for(i = 0; i < cantidadDeCalificaciones; i++){
-        let califEval = parseFloat(prompt(`¿Cuánto sacó ${nombre} en su examen número ${i+1}`))
+        let califEval = parseFloat(prompt(`¿Cuánto sacó ${nombre} en su examen número ${i+1}`));
         while(isNaN(califEval) || califEval < 0 || califEval > 10){
-            califEval = parseFloat(prompt("Lo siento, tienes que ingresar un número (no letras) entre el 0 y el 10 para continuar"))
+            califEval = parseFloat(prompt("Lo siento, tienes que ingresar un número (no letras) entre el 0 y el 10 para continuar"));
         }
     calificaciones.push(califEval);
 
@@ -39,7 +39,23 @@ function apruebaONo(promedio){
     } else if(promedio < 6){
         pruebaDeFuego = "Reprobado";
     } else{
-        alert("Error en la decisión de aprobar")
+        alert("Error en la decisión de aprobar");
     }
     return pruebaDeFuego;
+}
+
+
+function califDeMaterias(alumno, materias){
+
+    let caDeMat = [];
+    for(let i = 0; i < materias.length; i++){
+        califn = parseFloat(prompt(`¿Cuánto sacó ${alumno} en ${materias[i]}`));
+        while(isNaN(califn) || califn < 0 || califn > 10){
+            califn = parseFloat(prompt("Lo siento, tienes que ingresar un número (no letras) entre el 0 y el 10 para continuar"));
+        }
+        caDeMat.push(califn)
+    }
+
+    return caDeMat;
+
 }
--- Objetos Armando.js
+++ Objetos Armando.js
@@ -4,20 +4,14 @@ cada alumno tiene un nombre y una lista de calificaciones que se tienen que prom
  que no aprobaron.*/
 /*<button onclick="function()">name of button</button>*/ 
 
-/*
-function alumnos() {
-    let alumnos = [{
-        nombre: prompt("¿Cómo se llama tu alumno"),
-        calificaciones: lasCalificaciones(this.nombre),
-       // promedio: elPromedio(this.calificaciones)
-    }]
 
-    console.log(alumnos);}*/
 function alumnos(){
     let alumnos = []
    
     let cantidadDeAlumnos = parseInt(prompt("¿Cuántos alumnos tienes?"));
- 
+
+    let reprobados = [];
+
     while(isNaN(cantidadDeAlumnos)){
         cantidadDeAlumnos = parseInt(prompt("Lo siento, necesitas insertar un número para indicar la cantidad de alumnos"))
     }
@@ -38,8 +32,6 @@ function alumnos(){
         alumnos.push(estudiante)
     }
     
-    let reprobados = [];
-
     for(let i = 0; i < alumnos.length; i++){
         if(alumnos[i].estatus == "Reprobado"){
             reprobados.push(alumnos[i].nombre)
@@ -52,5 +44,49 @@ function alumnos(){
         console.log("Ningún alumno reprobado")
     }
     console.log(alumnos);
+
+}
+
+function joelCalif() {
+    
+    let alumnos = []
+   
+    let cantidadDeAlumnos = parseInt(prompt("¿Cuántos alumnos tienes?"));
+ 
+    while(isNaN(cantidadDeAlumnos)){
+        cantidadDeAlumnos = parseInt(prompt("Lo siento, necesitas insertar un número para indicar la cantidad de alumnos"));
+    }
+
+    let cantidadMaterias = parseInt(prompt("¿Cuántas materias impartes?"));
+
+    while(isNaN(cantidadMaterias)){
+        cantidadMaterias = parseInt(prompt("Número inválido, por favor, indica tu cantidad de materias usando números."));
+    }
+
+    let todasLasMaterias = [];
+
+    for(let i = 0; i < cantidadMaterias; i++){
+        todasLasMaterias.push(prompt(`¿Cómo se llama la materia número ${i + 1}`));
+    }
+
+    for(let i = 0; i < cantidadDeAlumnos; i++){
+ 
+        let estudiante = {};
+        let nombre =  prompt(`¿Cómo se llama tu alumno número ${i+1}`);
+        let evaluaciones = califDeMaterias(nombre, todasLasMaterias);
+        let esCalif = [];
+
+        for(let cont = 0;cont < todasLasMaterias.length; cont++ ){
+            esCalif.push(todasLasMaterias[cont], [evaluaciones[cont]])
+        }
+
+        estudiante.nombre = nombre;
+        estudiante.calificaciones = esCalif;
+
+       alumnos.push(estudiante)
+   }
+
+
+   console.log(alumnos)
 
 }
